9e001b62c5b6036bcd993703fb7d32ac6f7c7ac1
CyberPuck <traverskyle9@gmail.com>
2020-12-29 00:52:10 -0600

    Application CLI has been implemented
    CLI parsing is handled in main.rs with Clap.  Nannou GUI is now in
    src/gui.rs.  The GUI handling includes the addition of a static memory
    area for CLI parameters so data can be passed into the Nannou model.

--- src/main.rs
+++ src/main.rs
@@ -1,158 +1,73 @@
-/*use std::env;
-
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        print_help();
-        return
-    }
-    let filename = &args[1];
-    // TODO: Implement the following
-    //read_input_file(filename);
-    // let game = create_game(filename);
-    // game.run();
-}
-
-fn print_help() {
-    unimplemented!();
-}
-*/
 #[macro_use]
 extern crate clap;
-mod conway_engine;
-mod grid;
-use nannou::prelude::*;
-use std::time::Duration;
-
-struct Model {
-    engine: conway_engine::ConwayEngine,
-    window_height: f32,
-    window_width: f32,
-    time: Duration,
-}
+mod gui;
 
 fn main() {
     // handle CLI args
     let yaml = load_yaml!("cli.yml");
     // try setting the name, version
     let cli_app = clap::App::from_yaml(yaml);
-    let matches = cli_app.author(crate_authors!()).version(crate_version!()).about(crate_description!()).get_matches();
+    let matches = cli_app
+        .author(crate_authors!())
+        .version(crate_version!())
+        .about(crate_description!())
+        .get_matches();
 
-    println!("{:?}", matches);
+    // read in height and width, deafult is 1024 x 768
+    let height = matches
+        .value_of("height")
+        .unwrap_or("768")
+        .parse::<u32>()
+        .expect("Failed to parse height argument");
 
-    
-    nannou::app(model)
-        //.event(event)
-        .update(update)
-        .simple_window(view)
-        .run();
-}
+    let width = matches
+        .value_of("width")
+        .unwrap_or("1024")
+        .parse::<u32>()
+        .expect("Failed to parse width argument");
 
-fn model(app: &App) -> Model {
-    // setup the game
-    let window_data = app.main_window().rect();
-    let engine = conway_engine::ConwayEngine::new(
-        &"test-files/glider_test.txt".to_string(),
-        window_data.h(),
-        window_data.w(),
+    // read in the update rate
+    let update_rate = matches
+        .value_of("rate")
+        .unwrap_or("1")
+        .parse::<usize>()
+        .expect("Failed to parse rate argument");
+
+    // read in the number of steps
+    let number_of_steps = matches
+        .value_of("steps")
+        .unwrap_or("20")
+        .parse::<usize>()
+        .expect("Failed to parse number of steps argument");
+
+    // read in the alive color
+    // NOTE: All colors must be in lowercase to be parsed by the palette crate
+    let alive_color = matches
+        .value_of("alive")
+        .unwrap_or("BLACK")
+        .to_ascii_lowercase();
+
+    // read in the dead color
+    // NOTE: All colors must be in lowercase to be parsed by the palette crate
+    let dead_color = matches
+        .value_of("dead")
+        .unwrap_or("WHITE")
+        .to_ascii_lowercase();
+
+    // read in the game file, default is empty (which will generate a default oscillator)
+    let file_location = matches.value_of("file").unwrap_or("");
+
+    let enable_grid = matches.is_present("grid");
+
+    // Call the GUI class (empty struct with functions) to start the application
+    gui::GUI::start(
+        file_location.to_string(),
+        number_of_steps,
+        update_rate,
+        height,
+        width,
+        alive_color.to_string(),
+        dead_color.to_string(),
+        enable_grid,
     );
-    Model {
-        engine,
-        window_height: window_data.h(),
-        window_width: window_data.w(),
-        time: Duration::new(0, 0),
-    }
-}
-
-fn event(_app: &App, _model: &mut Model, _event: Event) {
-    unimplemented!();
-}
-
-fn update(_app: &App, model: &mut Model, _update: Update) {
-    // use _update.since_last as how long it has been since last step
-    model.time += _update.since_last;
-    if model.time > model.engine.get_update_rate_duration() {
-        model.engine.take_step();
-        model.time = Duration::new(0, 0);
-    }
-}
-
-fn view(app: &App, model: &Model, frame: Frame) {
-    // get canvas to draw on
-    let draw = app.draw();
-
-    // set background to blue
-    draw.background().color(WHITE);
-
-    // Draw the scene
-    draw_scene(model, &draw);
-    // test drawing checkerboard
-    //draw_checkboard(model, &draw);
-
-    // put everything on the frame
-    draw.to_frame(app, &frame).unwrap();
-}
-
-/// Draws cells based on if they are > 1
-fn draw_scene(model: &Model, draw: &Draw) {
-    let (row_width, column_width) = model.engine.get_grid_spacing();
-    let (row_count, column_count) = model.engine.get_grid_dimensions();
-    for row_number in 0..row_count {
-        for column_number in 0..column_count {
-            let (x, y) = convert_coordinates(row_number, column_number, model);
-            if model.engine.get_cell(row_number, column_number) > 0 {
-                draw.rect()
-                    .color(BLACK)
-                    .w(row_width - 1.0)
-                    .h(column_width - 1.0)
-                    .x_y(x + 0.5, y + 0.5);
-            }
-        }
-    }
-}
-
-#[allow(dead_code)]
-/// Simple test function that will print out a red and black checkerboard
-/// This function is for integration testing, it is not used during normal operations.
-fn draw_checkboard(model: &Model, draw: &Draw) {
-    let (row_width, column_width) = model.engine.get_grid_spacing();
-    let (row_count, column_count) = model.engine.get_grid_dimensions();
-    for row_number in 0..row_count {
-        for column_number in 0..column_count {
-            let (x, y) = convert_coordinates(row_number, column_number, model);
-            if (row_number % 2 == 0 && column_number % 2 == 0)
-                || (row_number % 2 != 0 && column_number % 2 != 0)
-            {
-                draw.rect()
-                    .color(BLACK)
-                    .w(row_width - 1.0)
-                    .h(column_width - 1.0)
-                    .x_y(x + 0.5, y + 0.5);
-            } else {
-                draw.rect()
-                    .color(RED)
-                    .w(row_width - 1.0)
-                    .h(column_width - 1.0)
-                    .x_y(x + 0.5, y + 0.5);
-            }
-        }
-    }
-}
-
-/// Given the row and column index, calculate the center draw position.
-/// Return float values as (X, Y).
-/// # Params
-/// - row_index: usize, row index in the grid
-/// - Column_index: usize, column index in the grid
-/// - model: &Model, model that contains the engine and grid dimensions
-/// # Returns
-/// - (f32, f32), (X, Y) screen coordinates for the given grid cell
-fn convert_coordinates(row_index: usize, column_index: usize, model: &Model) -> (f32, f32) {
-    let lower_x = (model.window_width / 2.0) * -1.0;
-    let lower_y = (model.window_height / 2.0) * -1.0;
-
-    let (x_width, y_width) = model.engine.get_grid_spacing();
-    let coordinate_x = lower_x + (column_index as f32 * x_width + x_width / 2.0);
-    let coordinate_y = (-1.0 * lower_y) - (row_index as f32 * y_width + y_width / 2.0);
-    (coordinate_x, coordinate_y)
 }
